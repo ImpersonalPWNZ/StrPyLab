1a3073531ad37b9d0eba03147c3d1ed35933c294
ImpersonalPWNZ <prixfe@gmail.com>
2022-04-29 20:44:23 +0300

    Lab2 Commit

--- main.py
+++ main.py
@@ -1,32 +1,56 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
 def StrLen(str):
-    return len(str)
+    messagebox.showinfo("Длина строки",f"Длина строки = {len(str)}")
 def SubstrInStr(str,sub):
-    return str.count(sub)
+    messagebox.showinfo("Подсчет подстроки", f"Подстрока {sub} встречается в строке {str} {str.count(sub)} раз")
 def WordCountInStr(str):
-    return len(str.split())
+    messagebox.showinfo("Количество слов", f"Количество слов в строке {str} = {len(str.split())}")
 def StrType(str):
     str = str.replace(" ","")
     if str.isdigit():
-        return "Digit"
+        res = "В строке содержатся только цифры"
     elif str.isalpha():
-        return "Alphabet only"
+        res = "В строке содержатся только буквы"
     elif str.isalnum():
-        return "Alphabet+Digit"
+        res = "В строке содержатся буквы и цифры"
     elif str =="":
-        return "Null string"
+        res = "Строка пустая"
     else:
-        return "With special symbols"
+        res = "В строке содержатся специальные символы"
+    messagebox.showinfo("Тип строки", f"{res}")
 def StrFormat(str):
     if str.isupper():
-        return "Upper"
+        res = "Только заглавные символы"
     elif str.islower():
-        return "Lower"
+        res = "Только строчные символы"
     else:
-        return "Mixed"
+        res = "И заглавные и строчные символы"
+    return messagebox.showinfo("Формат строки", f"{res}")
 
 
 if __name__ == '__main__':
-    str = 'ASD'
-    print(StrFormat(str))
+    window = tk.Tk()
+
+    MainLabel = Label(text="Введите строку")
+    StrEntry = Entry()
+    SubLabel = Label(text="Введите подстроку")
+    Sub = Entry()
+    btn_len = Button(master=window, text="Длина строки", command=lambda: StrLen(StrEntry.get()))
+    btn_substr = Button(master=window, text="Подсчет подстрок в строке", command=lambda: SubstrInStr(StrEntry.get(),Sub.get()))
+    btn_word = Button(master=window, text="Количество слов в строке", command=lambda: WordCountInStr(StrEntry.get()))
+    btn_type = Button(master=window, text="Тип строки", command=lambda: StrType(StrEntry.get()))
+    btn_format = Button(master=window, text="Формат строки", command=lambda: StrFormat(StrEntry.get()))
+    MainLabel.pack()
+    StrEntry.pack()
+    SubLabel.pack()
+    Sub.pack()
+    btn_len.pack()
+    btn_substr.pack()
+    btn_word.pack()
+    btn_type.pack()
+    btn_format.pack()
+    window.mainloop()
 
 
